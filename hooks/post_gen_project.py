b7987db55723eb767aa77809d51c401bba42d8a0
djrlj694 <robert.l.jones@synthelytics.com>
2019-05-31 17:59:52 -0400

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -46,6 +46,9 @@ if '{{ cookiecutter.license }}' != 'Not open source':
 if {{ cookiecutter.include_makefile }}:
     add('makefile', extra_context)
 
+if '{{ cookiecutter.project_type }}' == 'Xcode':
+    add('xcode', extra_context)
+
 # Create project from the cookiecutter-pypackage.git repo template.
 #cookiecutter(
 #    'gh:djrlj694/Cookiecutter-Xcode',
