b7ec9dd8dceac007dc56e6fb885aaae268c30f2e
djrlj694 <robert.l.jones@synthelytics.com>
2019-06-03 06:27:43 -0400

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,7 +3,7 @@
 
 __author__ = 'Robert (Bob) L. Jones'
 __coauthor__ = 'N/A'
-__copyright__ = 'Copyright 2019, Cookiecutter-Project'
+__copyright__ = 'Copyright 2019, Cookiecutter-Repo'
 __credits__ = ['Robert (Bob) L. Jones']
 __license__ = 'MIT'
 __version__ = '0.0.1'
@@ -11,7 +11,7 @@ __maintainer__ = 'Robert (Bob) L. Jones'
 __email__ = 'djrlj694@gmail.com'
 __status__ = 'Development'
 __created_date__= 'May 27, 2019'
-__modified_date__= 'May 30, 2019'
+__modified_date__= 'Jun 3, 2019'
 
 ### Libraries ###
 
@@ -28,6 +28,7 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 ### Function Declarations ###
 
+# Create a repo from a cookiecutter.
 def add(cookiecutter_suffix, extra_context):
     cookiecutter(
         'gh:djrlj694/cookiecutter-%s' % cookiecutter_suffix,
@@ -38,22 +39,17 @@ def add(cookiecutter_suffix, extra_context):
 
 ### Main Program ###
 
-extra_context = {'project_name': '{{ cookiecutter.project_name }}'}
+extra_context = {'project_name': '{{ cookiecutter.repo_formal_name }}'}
 
-if '{{ cookiecutter.license }}' != 'Not open source':
+if '{{ cookiecutter.repo_license }}' != 'Not open source':
     add('github', extra_context)
 
 if {{ cookiecutter.include_makefile }}:
     add('makefile', extra_context)
 
-if '{{ cookiecutter.project_type }}' == 'iOS':
+if '{{ cookiecutter.repo_project_type }}' in ['iOS', 'macOS', 'Swift', 'tvOS', 'watchOS']:
     add('xcode', extra_context)
-
-# Create project from the cookiecutter-pypackage.git repo template.
-#cookiecutter(
-#    'gh:djrlj694/Cookiecutter-Xcode',
-#    extra_context={'project_name': '{{ cookiecutter.project_name }}'},
-#    output_dir='..',
-#    overwrite_if_exists=True)
+if '{{ cookiecutter.repo_project_type }}' == 'Python':
+    add('python', extra_context)
 
 sys.exit(0)
