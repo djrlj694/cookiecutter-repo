f7db4b75a1098acdea0597eae5e8602948de59e6
djrlj694 <robert.l.jones@synthelytics.com>
2019-06-11 19:08:16 -0400

    Create formatting.mk

---
+++ {{cookiecutter.repo_dir}}/.make/features/formatting.mk
@@ -0,0 +1,41 @@
+# Copyright © 2019 djrlj694.dev. All rights reserved.
+#==============================================================================#
+# PROGRAM: formatting.mk
+# AUTHORS: Robert L. Jones
+# COMPANY: djrlj694.dev
+# VERSION: 1.0.0
+# CREATED: 16MAR2019
+# REVISED: 25MAY2019
+#==============================================================================#
+# For more info on terminology, style conventions, or source references, see
+# the file ".make/README.md".
+#==============================================================================#
+
+#==============================================================================#
+# INTERNAL CONSTANTS
+#==============================================================================#
+
+#------------------------------------------------------------------------------#
+# Special characters
+#------------------------------------------------------------------------------#
+
+# C-style octal code representing an ASCI escape character.
+ESC := \033
+
+EMPTY :=
+SPACE := $(EMPTY) $(EMPTY)
+
+#------------------------------------------------------------------------------#
+# ANSI escape sequences
+#------------------------------------------------------------------------------#
+
+# Setting the text intensity/emphasis of STDOUT.
+RESET := $(ESC)[0m
+BOLD := $(ESC)[1m
+DIM := $(ESC)[2m
+
+# Setting the text color of STDOUT.
+FG_CYAN := $(ESC)[0;36m
+FG_GREEN := $(ESC)[0;32m
+FG_RED := $(ESC)[0;31m
+FG_YELLOW := $(ESC)[1;33m
