020f8d33bcfe20a6f5007fdd193e73f1c16cd758
djrlj694 <robert.l.jones@synthelytics.com>
2019-06-11 19:08:33 -0400

    Create Cookiecutter.mk

---
+++ {{cookiecutter.repo_dir}}/.make/platforms/Cookiecutter.mk
@@ -0,0 +1,70 @@
+# Copyright © 2019 djrlj694.dev. All rights reserved.
+#==============================================================================#
+# PROGRAM: Cookiecutter.mk
+# AUTHORS: Robert L. Jones
+# COMPANY: djrlj694.dev
+# VERSION: 1.0.0
+# CREATED: 10MAR2019
+# REVISED: 27MAY2019
+#==============================================================================#
+# For more info on terminology, style conventions, or source references, see
+# the file ".make/README.md".
+#==============================================================================#
+
+#==============================================================================#
+# EXTERNAL CONSTANTS
+#==============================================================================#
+
+#------------------------------------------------------------------------------#
+# Command output
+#------------------------------------------------------------------------------#
+
+COOKIECUTTER ?= $(shell which cookiecutter)
+
+#==============================================================================#
+# INTERNAL CONSTANTS
+#==============================================================================#
+
+#------------------------------------------------------------------------------#
+# Debugging & error capture
+#------------------------------------------------------------------------------#
+
+VARIABLES_TO_SHOW += COOKIECUTTER
+
+#------------------------------------------------------------------------------#
+# Help strings
+#------------------------------------------------------------------------------#
+
+MAKE_ARGS += [COOKIECUTTER=]
+
+#==============================================================================#
+# USER-DEFINED FUNCTIONS
+#==============================================================================#
+
+# $(call sed-cmd,cc_var,replacement)
+# Generates a sed command for substituting a Cookiecutter template variable with
+# a replacement value.
+# FIXME: Not escaping the curly braces results in the following error during
+# FIXME: the execution of Cookiecutter:
+# FIXME: jinja2.exceptions.TemplateSyntaxError: unexpected char '$' at 1953
+# FIXME:   File "./.make/platforms/Cookiecutter.mk", line 48
+ifeq ($(COOKIECUTTER),)
+cc-sed-cmd = s/\{\{ cookiecutter.$1 \}\}/$2/g
+endif
+
+#==============================================================================#
+# UTILITY DEPENDENCIES
+#==============================================================================#
+
+ifeq ($(COOKIECUTTER),)
+include $(MAKEFILE_DIR)/utilities/sed.mk
+endif
+
+#==============================================================================#
+# FEATURE DEPENDENCIES
+#==============================================================================#
+
+ifeq ($(COOKIECUTTER),)
+include $(MAKEFILE_DIR)/features/downloading.mk
+include $(MAKEFILE_DIR)/features/setting_up.mk
+endif
