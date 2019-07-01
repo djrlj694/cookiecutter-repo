94fbec255b9766f759de1292b5e6b5029f877068
djrlj694 <robert.l.jones@synthelytics.com>
2019-07-01 17:54:50 -0400

    Update Swift.mk

--- .make/platforms/Swift.mk
+++ .make/platforms/Swift.mk
@@ -5,7 +5,7 @@
 # COMPANY: djrlj694.dev
 # VERSION: 1.0.0
 # CREATED: 03MAR2019
-# REVISED: 15JUN2019
+# REVISED: 01JUL2019
 #==============================================================================#
 # For more info on terminology, style conventions, or source references, see
 # the file ".make/README.md".
@@ -45,8 +45,8 @@ ifeq ($(COOKIECUTTER),)
 init-swift: init-swift-vars init-swift-dirs init-carthage init-cocoapods
 else
 init-swift: init-swift-vars
-#	@cookiecutter gh:$(TEMPLATES_REPO) project_name=$(PROJECT)
-	swift package init
+	@cookiecutter -f gh:$(TEMPLATES_REPO) project_name=$(PROJECT)
+###	swift package init
 endif
 
 ## init-swift-dirs: Completes all initial Swift directory setup activites.
