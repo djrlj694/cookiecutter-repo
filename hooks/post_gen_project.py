e875718fcb088801ca2217d31443cf21b64a986a
djrlj694 <robert.l.jones@synthelytics.com>
2019-07-07 09:03:45 -0400

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -49,6 +49,7 @@ extra_context = {'project_name': '{{ cookiecutter.repo_name }}'}
 
 platform = '{{ cookiecutter.repo_platform }}'.split(' ')[0].lower
 if platform == 'swift':
+    os.system('echo platform=swift')
     swift_package_type = '{{ cookiecutter.repo_platform }}'.split(' ')[1]
     os.system('make init-swift SWIFT_PACKAGE_TYPE={}'.format(swift_package_type))
     #os.system('make init-swift SWIFT_PACKAGE_TYPE=%s' % (swift_package_type))
