d6794ce010b9215547868e2b3418c9ea86814d93
djrlj694 <robert.l.jones@synthelytics.com>
2019-12-13 07:55:40 -0500

    Update post_gen_project.py

--- {{cookiecutter.repo_dir}}/.boilerplate/cookiecutter_template/hooks/post_gen_project.py
+++ {{cookiecutter.repo_dir}}/.boilerplate/cookiecutter_template/hooks/post_gen_project.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__ = '{{ cookiecutter.lead_name }}'
-__coauthor__ = 'N/A'
-__copyright__ = 'Copyright {% now 'local', '%Y' %}, {{ cookiecutter.repo_name }}'
-__credits__ = ['{{ cookiecutter.lead_name }}']
-__license__ = '{{ cookiecutter.repo_license }}'
-__version__ = '0.0.1'
-__maintainer__ = '{{ cookiecutter.lead_name }}'
-__email__ = '{{ cookiecutter.lead_email }}'
-__status__ = 'Development'
+__version__ = '0.0.0'
+
+__author__ = '{{cookiecutter.lead_name}}'
+__credits__ = ['{{cookiecutter.lead_name}}']
+
+__copyright__ = 'Copyright {% now 'local', '%Y' %}, {{cookiecutter.repo_name}}'
+__license__ = '{{cookiecutter.repo_license}}'
+
 __created_date__= '{% now 'local', '%b %d, %Y' %}'
 __modified_date__= '{% now 'local', '%b %d, %Y' %}'
 
