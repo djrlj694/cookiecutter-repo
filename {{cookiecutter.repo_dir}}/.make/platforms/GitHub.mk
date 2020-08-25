fbc973c74b92755a3b6866d04961c8c9e70195a2
Robert (Bob) L. Jones <3848052+djrlj694@users.noreply.github.com>
2020-08-25 07:39:31 -0400

    Update GitHub.mk

--- {{cookiecutter.repo_dir}}/.make/platforms/GitHub.mk
+++ {{cookiecutter.repo_dir}}/.make/platforms/GitHub.mk
@@ -58,6 +58,10 @@ clean-docs-github: | $(LOG)
 ## init-github: Completes all initial Github setup activites.
 init-github:
 	@printf "Initializing GitHub repository..."
+	$(eval description_kv = "description":"This is a text")
+	$(eval license_kv = "license_template":"$(LICENSE_TEMPLATE)")
+	$(eval name_kv = "name":"$(PROJECT)")
+	$(eval private_kv = "private":"$(IS_PRIVATE)")
 	$(eval gh_api_url = https://api.github.com/user/repos)
 	$(eval PROJECT_REPO = $(GITHUB_USER)/$(PROJECT))
 	$(eval TEMPLATES_REPO = $(GITHUB_USER)/cookiecutter-github)
@@ -65,12 +69,16 @@ init-github:
 #	@cookiecutter gh:$(TEMPLATES_REPO) email=$(EMAIL) project_name=$(PROJECT)
 #	@curl -H "Authorization: token $(GITHUB_API_TOKEN)" https://api.github.com/user/repos -d '{"name": "'"${NEW_REPO_NAME}"'"}'
 
+	#@curl -u $(GITHUB_USER) $(gh_api_url) \
+	#-d '{"name": "$(PROJECT)", "description": "This is a text", "private": $(IS_PRIVATE), "license_template": "$(LICENSE_TEMPLATE)"}'; \
+	#$(status_result)
+
 ifeq ($(LICENSE),)
 	@curl -u $(GITHUB_USER) $(gh_api_url) \
-	-d '{"name": "$(PROJECT)", "description": "This is a text", "private": $(IS_PRIVATE), "license_template": "$(LICENSE_TEMPLATE)"}'; \
+	-d '{$(name_kv),$(description_kv),$(private_kv),$(license_kv)}'; \
 	$(status_result)
 else
 	@curl -u $(GITHUB_USER) $(gh_api_url) \
-	-d '{"name": "$(PROJECT)", "description": "This is a text", "private": $(IS_PRIVATE)}'; \
+	-d '{$(name_kv),$(description_kv),$(private_kv)'; \
 	$(status_result)
 endif
