dd1b3e8a232c7af70c4f50104a6c301365ad604d
Robert (Bob) L. Jones <3848052+djrlj694@users.noreply.github.com>
2020-09-02 08:31:50 -0400

    Update {{cookiecutter.target_name}}Tests.swift

--- {{cookiecutter.repo_dir}}/.boilerplate/Swift/package/Tests/{{cookiecutter.target_name}}Tests/{{cookiecutter.target_name}}Tests.swift
+++ {{cookiecutter.repo_dir}}/.boilerplate/Swift/package/Tests/{{cookiecutter.target_name}}Tests/{{cookiecutter.target_name}}Tests.swift
@@ -1,2 +1,2 @@
 {% extends ".boilerplate/swift/template.swift" %}
-{% block file_name %}{{cookiecutter.target_name}}Tests.swift{% endblock %}
+{% block file_name %}{{cookiecutter.project_name}}Tests.swift{% endblock %}
