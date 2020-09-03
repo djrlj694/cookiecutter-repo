57db479986ce305f051283f84dc39b45e06dbdbe
Robert (Bob) L. Jones <3848052+djrlj694@users.noreply.github.com>
2020-09-03 07:46:57 -0400

    Update files

---
+++ {{cookiecutter.repo_dir}}/.boilerplate/Swift/template.swift
@@ -0,0 +1,8 @@
+//
+//  {% block file_name %}{% endblock %}
+//  {{cookiecutter.project_name}}
+//
+//  Created by {{cookiecutter.lead_name}} on {% now 'local', '%-m/%-d/%y' %}.
+//  Copyright © {% now 'local', '%Y' %} {{cookiecutter.organization_name}}. All rights reserved.
+//
+{% block body %}{% endblock %}
